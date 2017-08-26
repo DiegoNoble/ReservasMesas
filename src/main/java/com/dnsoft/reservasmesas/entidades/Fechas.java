81cd95a3172f724522a27e25310f171797f06726
Diego <Diego@DiegoNoble>
2017-08-26 14:32:28 -0300

    ok

---
+++ src/main/java/com/dnsoft/reservasmesas/entidades/Fechas.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dnsoft.reservasmesas.entidades;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Diego
+ */
+@Entity
+@Table(name = "fechas")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Fechas.findAll", query = "SELECT f FROM Fechas f")})
+public class Fechas implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Long id;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "fecha")
+    @Temporal(TemporalType.DATE)
+    private Date fecha;
+
+    public Fechas() {
+    }
+
+    public Fechas(Long id) {
+        this.id = id;
+    }
+
+    public Fechas(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Fechas(Long id, Date fecha) {
+        this.id = id;
+        this.fecha = fecha;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Fechas)) {
+            return false;
+        }
+        Fechas other = (Fechas) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.dnsoft.reservasmesas.entidades.Fechas[ id=" + id + " ]";
+    }
+    
+}
